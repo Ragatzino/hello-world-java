cbc722bbc1a68d977cde4513b362f72790a18e4e
ragatzino <antoinebrunetti@gmail.com>
2022-03-10 23:17:19 +0100

    TTL in job and not in java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,5 @@ public class App
 {
     public static void main( String[] args ) throws InterruptedException {
         System.out.println( "Hello World!" );
-        Thread.sleep(10000L);
-        System.out.println("end");
     }
 }
